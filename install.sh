64506d6c726063894276d19df6a4fdfdc9bc29fb
Tomasz Trębski <kornicameister@gmail.com>
2020-05-08 07:49:06 +0200

    Add basic oneliner installation

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if command -v docker >/dev/null 2>&1; then
+  dir="$(mktemp -t -d hadolint-shim.XXXXXXXXXX)"
+  
+  curl -sSL \
+    -o "${dir}/hadolint"
+    https://raw.githubusercontent.com/kornicameister/hadolint-shim/master/hadolint
+    
+  chmod +x "${dir}/hadolint"
+  sudo mv "${dir}/hadolint" "${PREFIX-/usr/local/bin}/hadolint"
+  
+  rm -rf "${dir}"
+else
+
+  echo "docker is required for hadolint-shim to work"
+  exit 1
+
+fi
