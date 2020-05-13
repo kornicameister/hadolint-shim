08580c7416dc2ff87f5a6e17745a1c7734ca52ab
Tomasz Trębski <kornicameister@gmail.com>
2020-05-13 19:15:56 +0200

    Install fixes (#2)

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+test -n ${INSTALL_TRACE} && set -x
+
 if command -v docker >/dev/null 2>&1; then
+
   dir="$(mktemp -t -d hadolint-shim.XXXXXXXXXX)"
 
   curl -sSL \
     -o "${dir}/hadolint" \
     https://raw.githubusercontent.com/kornicameister/hadolint-shim/master/hadolint
 
-  chmod +x "${dir}/hadolint"
-  cp -f "${dir}/hadolint" "${PREFIX-/usr/local}/bin/hadolint"
-
+  installDir="${PREFIX-/usr/local}/bin" && mkdir -p "${installDir}"
+  chmod +x "${dir}/hadolint" && cp -f "${dir}/hadolint" "${installDir}/hadolint"
   rm -rf "${dir}"
+
 else
 
   echo "docker is required for hadolint-shim to work"
